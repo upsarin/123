50b92af2a1be66e72a2a9f89a4a932915e05d22e
upsarin <mvlad91@mail.ru>
2017-04-08 21:22:18 +0700

    v. 2.2.8 some edits for safari

--- js/gallery.js
+++ js/gallery.js
@@ -4,37 +4,41 @@ $(document).ready(function(){
         var obj = $("#gallery-" + id);
         var subj = $(".gallery-showcase-block");
         var imgMap = $(".gallery-cont-"+ id +" img");
-        subj.animate({
-            opacity: 1,
-            height: $(".gallery-cont-"+ id +" img")[0].height
-        }, 200, function(){
-            subj.html('<img src="'+ $(".gallery-cont-"+ id +" img")[0].src +'" />');
-            $(".gallery-cont-moscow").animate({
-                height: $(".gallery-cont-"+ id +" img")[1].height/1.5 + "px",
+        if($(".gallery-cont-" + id + " .imgMap").height > 0) {
+            subj.animate({
                 opacity: 1,
-                width: "100%"
-            }, 200, function(){
-                $(".gallery-header-"+ id).css({
-                    margin: "0px"
-                });
-                $(".gallery-cont-"+ id +" .imgMap").animate({
-                    height: this.height,
+                height: $(".gallery-cont-" + id + " img")[0].height
+            }, 200, function () {
+                subj.html('<img src="' + $(".gallery-cont-" + id + " img")[0].src + '" />');
+                $(".gallery-cont-" + id).animate({
+                    height: $(".gallery-cont-" + id + " img")[1].height / 1.5 + "px",
                     opacity: 1,
-                    width: "100%",
-                }, 200, function(){
-                    $(".gallery-cont-"+ id).css({
-                        margin: "0px 0px 10px 0px"
+                    width: "100%"
+                }, 200, function () {
+                    $(".gallery-header-" + id).css({
+                        margin: "0px"
                     });
-                    $(".points-" + id + " .point").css({
-                        display: "block"
-                    });
-                    
-                    var destination = $(".gallery-showcase-block").offset().top;
-                    $('body').animate({ scrollTop: destination-100 }, 1100);
-                });
+                    $(".gallery-cont-" + id + " .imgMap").animate({
+                        height: this.height,
+                        opacity: 1,
+                        width: "100%",
+                    }, 200, function () {
+                        $(".gallery-cont-" + id).css({
+                            margin: "0px 0px 10px 0px"
+                        });
+                        $(".points-" + id + " .point").css({
+                            display: "block"
+                        });
 
+                        var destination = $(".gallery-showcase-block").offset().top;
+                        $('body').animate({scrollTop: destination - 100}, 1100);
+                    });
+
+                });
             });
-        });
+        } else {
+
+        }
         console.log("gallery test");
     });
 });
